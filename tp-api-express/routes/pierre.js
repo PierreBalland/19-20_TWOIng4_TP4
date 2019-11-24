f94287fe356330182133eeed0f7092869b2bb3c8
Pierre <pierre.balland@edu.ece.fr>
2019-11-24 18:02:34 +0100

    Struct buggé

---
+++ tp-api-express/routes/pierre.js
@@ -0,0 +1,40 @@
+const express = require('express');
+const hostname = 'localhost';
+const router = express.Router();
+const port =3000;
+
+const axios = require('axios');
+
+
+let users = [{
+  user: "Pierre",
+  id: "0"
+}];
+
+/* GET users listing. */
+router.get('/', (req, res) => {
+  // Get List of user and return JSON
+  axios.get('http://www.omdbapi.com/?t=inception&apikey=b3d7a4f5')
+    .then(function (response) {
+      // handle success
+
+      console.log(response);
+    })
+    .catch(function (error) {
+      // handle error
+      if(4==3)
+      {
+      res.status(200).json({ users });
+      console.log(error);
+      }
+    })
+    .finally(function () {
+      // always executed
+    });
+
+});
+	/*Put*/
+
+
+
+module.exports = router;
