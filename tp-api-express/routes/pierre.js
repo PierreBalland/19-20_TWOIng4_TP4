fb259bcdadbb2c69739f8271c79a2ab0251f1c80
Pierre <pierre.balland@edu.ece.fr>
2019-11-25 11:06:07 +0100

    Put qui marche avec req.params

--- tp-api-express/routes/pierre.js
+++ tp-api-express/routes/pierre.js
@@ -2,25 +2,33 @@ const express = require('express');
 const hostname = 'localhost';
 const router = express.Router();
 const port =3000;
+const _ = require('lodash');
 
 const axios = require('axios');
-
 
 let users = [{
   user: "Pierre",
   id: "0"
 }];
+let movies = [{
+  movie: "Pierre",
+  id: "0"
+}];
 
-/* GET users listing. */
-router.get('/', (req, res) => {
+/*GET users listing.
+router.get('/:id', (req, res) => {
   // Get List of user and return JSON
-  res.status(200).json({ users });
-  axios.get('http://www.omdbapi.com/?t=inception&apikey=b3d7a4f5')
+ res.status(200).json({ users });
+  const { id } = req.params;
+  const string = 'http://www.omdbapi.com/?t='+ id +'&apikey=b3d7a4f5';
+  axios.get(string)
     .then(function (response) {
       // handle success
+let use =response;
+      console.log(use);
 
-      console.log(response);
 
+    // res.status(200).json({ use });
     })
     .catch(function (error) {
       // handle error
@@ -33,8 +41,34 @@ router.get('/', (req, res) => {
     });
 
 });
-	/*Put*/
+*/
+/* PUT new movie. */
+router.put('/:moviename', (req, res) => {
+  // Get the data from request from request
 
+ const movie = req.params;
+console.log("bonjour");
+console.log(movie);
+//console.log(req.body);
+  // Create new unique id
+  const id = _.uniqueId();
+  // Insert it in array (normaly with connect the data with the database)
+  movies.push({ movie, id });
+  // Return message
+  res.json({message: `Just added ${id}`, movie: { movie, id } });
+});
+	/*
+	router.get('/:id', (req, res) => {
+      const { id } = req.params;
+      // Find user in DB
+      const user = _.find(users, ["id", id]);
+      // Return user
+      res.status(200).json({
+        message: 'User found!',
+        user
+      });
+    });
+*/
 
 
 module.exports = router;
